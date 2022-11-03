8b86fcc2c87476fd5c6ea5ad4c173d13487ef0f4
Amir Hamza <52718336+amirhamza24@users.noreply.github.com>
2022-11-03 14:48:45 +0600

    Add files via upload

---
+++ UserFormUsingIMG/JavaApplication33.java
@@ -0,0 +1,11 @@
+package javaapplication33;
+
+public class JavaApplication33 {
+
+    
+    public static void main(String[] args) {
+        user_form ob = new user_form();
+        ob.setVisible(true);
+    }
+    
+}
