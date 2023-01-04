63364bb204da0e4d7a140656ce123d46bca05f91
Amir Hamza <52718336+amirhamza24@users.noreply.github.com>
2023-01-05 01:11:09 +0600

    Update Task3.java

--- Assignment 1/Task3.java
+++ Assignment 1/Task3.java
@@ -1,41 +1,35 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
- */
 package task3;
 
 import java.util.Scanner;
 
-/**
- *
- * @author isratjisa
- */
 public class Task3 {
 
-    /**
-     * @param args the command line arguments
-     */
-    public static void main(String[] args) {
-        Scanner scn = new Scanner(System.in);
-        System.out.println("Enter Array size : ");
-        int n = scn.nextInt();
+    public static void main(String[] args) 
+    {
+        int count = 0, i;
+        Scanner s = new Scanner(System.in);
+
+        System.out.print("Enter array size: ");
+        int num = s.nextInt();
         
-        System.out.println("Enter the number to search : ");
-        int num = scn.nextInt();
+        int arr[] = new int[num];
         
-        int[] arr = new int[n];
-        int counter = 0;
-        
-        System.out.println("Please Enter " + n + "numbers : ");
-        for(int i = 0; i < n; i++){
-            arr[i] = scn.nextInt();
+        System.out.println("Enter the elements: ");
+        for(i = 0; i < num; i++)
+        {
+            arr[i] = s.nextInt();
         }
+        System.out.print("Enter the number to search: ");
+        int search = s.nextInt();
         
-        for(int i = 0; i < n; i++){
-            if(arr[i] == num) counter++;
+        for(i = 0; i < num; i++)
+        {
+            if(arr[i] == search)
+            {
+                count++;
+            }
         }
-        
-        System.out.println("Result: " + counter);
+        System.out.println(search+" " + "-"+" "+count);
     }
     
 }
